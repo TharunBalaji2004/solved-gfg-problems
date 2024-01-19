fb97ea0f75d2db051e68de4634831017fdd2a04d
Tharun Balaji R <95350584+TharunBalaji2004@users.noreply.github.com>
2024-01-19 19:57:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Top k numbers in a stream/top-k-numbers-in-a-stream.java
@@ -0,0 +1,131 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+
+class IntMatrix
+{
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException
+    {
+        int[][] mat = new int[n][];
+
+        for(int i = 0; i < n; i++)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for(int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+
+        return mat;
+    }
+
+    public static void print(int[][] m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+
+    public static void print(ArrayList<ArrayList<Integer>> m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            String St[] = br.readLine().split(" ");
+            int N = Integer.parseInt(St[0]);
+            int K = Integer.parseInt(St[1]);
+           
+            int[] arr = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer>> res = obj.kTop(arr, N, K);
+            
+            IntMatrix.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> kTop(int[] arr, int n, int k) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> Integer.compare(a[0], b[0]) != 0 ? Integer.compare(a[0], b[0]) : Integer.compare(b[1], a[1])
+            );
+
+            for (int key : map.keySet()) {
+                pq.add(new int[]{map.get(key), key});
+
+                if (pq.size() > k) {
+                    pq.poll(); 
+                }
+            }
+
+            ArrayList<Integer> list = new ArrayList<>();
+            while (!pq.isEmpty()) {
+                int[] pair = pq.poll();
+                list.add(pair[1]);
+            }
+            
+            Collections.reverse(list);
+
+            res.add(list);
+        }
+
+        return res;
+    }
+}
+        
